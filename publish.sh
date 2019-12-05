cc2442b1496c184822a1c006d281f2cc0938d5e3
alioualarbi <40361357+alioualarbi@users.noreply.github.com>
2019-12-04 21:35:05 -0500

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,19 +6,19 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [[ -n $TRAVIS_TAG ]]; then
     TAG=${TRAVIS_TAG/v/}
     echo "publish $TAG"
-	docker tag dockeralioua/Jenkins-dind dockeralioua/Jenkins-dind:${TAG}
-	docker tag dockeralioua/Jenkins-dind:alpine dockeralioua/Jenkins-dind:${TAG}-alpine
-	docker tag dockeralioua/Jenkins-dind:debian dockeralioua/Jenkins-dind:${TAG}-debian
-	docker tag dockeralioua/Jenkins-dind:jdk11 dockeralioua/Jenkins-dind:${TAG}-jdk11
-	docker push dockeralioua/Jenkins-dind:${TAG}
-	docker push dockeralioua/Jenkins-dind:${TAG}-alpine
-	docker push dockeralioua/Jenkins-dind:${TAG}-debian
-	docker push dockeralioua/Jenkins-dind:${TAG}-jdk11
+	docker tag alioualarbi/Jenkins-dind dockeralioua/jenkins-dind:${TAG}
+	docker tag alioualarbi/Jenkins-dind:alpine dockeralioua/jenkins-dind:${TAG}-alpine
+	docker tag alioualarbi/Jenkins-dind:debian dockeralioua/jenkins-dind:${TAG}-debian
+	docker tag alioualarbi/Jenkins-dind:jdk11 dockeralioua/jenkins-dind:${TAG}-jdk11
+	docker push alioualarbi/Jenkins-dind:${TAG}
+	docker push alioualarbi/Jenkins-dind:${TAG}-alpine
+	docker push alioualarbi/Jenkins-dind:${TAG}-debian
+	docker push alioualarbi/Jenkins-dind:${TAG}-jdk11
 
 else
     echo "publish latest"
-	docker push dockeralioua/Jenkins-dind
-	docker push dockeralioua/Jenkins-dind:alpine
-	docker push dockeralioua/Jenkins-dind:debian
-	docker push dockeralioua/Jenkins-dind:jdk11
+	docker push dockeralioua/jenkins-dind
+	docker push dockeralioua/jenkins-dind:alpine
+	docker push dockeralioua/jenkins-dind:debian
+	docker push dockeralioua/jenkins-dind:jdk11
 fi
