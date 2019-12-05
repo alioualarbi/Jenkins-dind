2df58d3ce3ab5adcc826722b8490201c625146c0
alioualarbi <40361357+alioualarbi@users.noreply.github.com>
2019-12-05 17:10:22 -0500

    Update modprobe.sh

--- modprobe.sh
+++ modprobe.sh
@@ -2,10 +2,6 @@
 set -eu
 
 # "modprobe" without modprobe
-# https://twitter.com/lucabruno/status/902934379835662336
-
-# this isn't 100% fool-proof, but it'll have a much higher success rate than simply using the "real" modprobe
-
 # Docker often uses "modprobe -va foo bar baz"
 # so we ignore modules that start with "-"
 for module; do
